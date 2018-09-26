ad49742af9c97d0c2ffcb54e70fb760d38c0beb6
AndyP12 <43144526+AndyP12@users.noreply.github.com>
2018-09-26 10:50:02 +0100

    Add files via upload

---
+++ UserDefinedMethod.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class UserDefinedMethod {
+
+
+    public static void main(String args[]){
+
+        int cube, number = 1;
+
+
+    }
+
+    public static int cube (int a){
+
+        int cube, number = 1;
+
+        for(int i = 1; i <= 15; i++) {
+            cube = number * number * number;
+            number++;
+        }
+
+    }
+}
